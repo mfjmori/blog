0fc4fd25eb108de13004d7d776062334080109ed
mfjmori <mfjmori@gmail.com>
2019-05-03 12:09:26 +0900

    Update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def active_controller_and_action
+    if params[:controller] == 'articles' && params[:action] == 'index'
+      "hide"
+    elsif params[:controller] == 'users' && params[:action] == 'show'
+      "hide"
+    else
+      nil
+    end
+  end
 end
