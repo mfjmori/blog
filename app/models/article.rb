fc6d061754ec9dd839fac3405e81a4cccdaa5f54
mfjmori <mfjmori@gmail.com>
2019-05-07 15:02:41 +0900

    add validates to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
+  validates :publish, inclusion: {in: [true, false]}
 end
